650b6591dabaac88deed59a0a050186d8c7fe5f4
enzohachi <enzo.jpq@gmail.com>
2020-08-27 19:39:38 -0400

    subir ejemplologin

---
+++ app/src/main/java/cl/grupo2/ejemplofirebase/MainActivity.kt
@@ -0,0 +1,11 @@
+package cl.grupo2.ejemplofirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/cl/grupo2/ejemplofirebase/registro/RegistroFragment.kt
@@ -0,0 +1,18 @@
+package cl.grupo2.ejemplofirebase.registro
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cl.grupo2.ejemplofirebase.R
+
+class RegistroFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registro, container, false)
+    }
+}
