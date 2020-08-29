f4a947a759935a644a3ee18cfe8c81bbbb67152b
enzohachi <enzo.jpq@gmail.com>
2020-08-29 09:40:50 -0400

    cambios

--- app/src/main/java/cl/grupo2/ejemplofirebase/registro/domain/RegistroUsuario.kt
+++ app/src/main/java/cl/grupo2/ejemplofirebase/registro/domain/RegistroUsuario.kt
@@ -3,6 +3,7 @@ package cl.grupo2.ejemplofirebase.registro.domain
 data class RegistroUsuario (
     val nombre: String,
     val rut: String,
-    val pass: String,
-    val email:String
+    val email : String,
+    val pass: String
+
 )
